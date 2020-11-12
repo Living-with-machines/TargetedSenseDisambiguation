06497a753510fd5c99cea995480c7fd417117a53
fedenanni <nanni.federico@gmail.com>
2020-11-12 09:46:18 +0000

    Add spacy nlp as a preprocessing step

--- tasks/wsd.py
+++ tasks/wsd.py
@@ -2,8 +2,8 @@ import spacy
 from random import shuffle
 from sklearn.metrics import precision_recall_fscore_support
 
-nlp = spacy.load("en_core_web_sm")
 
+### evaluation metrics
 def eval(ranking,gold):
     preds = []
     for line in ranking:
@@ -16,20 +16,33 @@ def eval(ranking,gold):
     microf1 = round(precision_recall_fscore_support(gold,preds, average='micro')[2],3)
     return p,r,f1,microf1
 
+
+### preprocessing (for the moment only this)
+nlp = spacy.load("en_core_web_sm", disable=['parser', 'ner'])
+
+def preprocess(text):
+    processed_text = nlp(text)
+    return processed_text
+
+
+### random baseline
 def random_predict(definition_df):
     sense_ids = definition_df["sense_id"].tolist()
     shuffle(sense_ids)
     results = {sense_ids[x]:len(sense_ids)-x for x in range(len(sense_ids))}
     return results
 
+### token overlap baseline
 def tok_overlap_ranking(sent,definition_df):
-    definition_df["tok_overlap"] = definition_df.apply (lambda row: token_overlap(sent,row["definition"]), axis=1)
+    definition_df["tok_overlap"] = definition_df.apply (lambda row: token_overlap(sent,row["nlp_definition"]), axis=1)
     results = definition_df.set_index('sense_id').to_dict()["tok_overlap"]
     return results
 
 def token_overlap(sent1,sent2):
-    # we could do this text processing before as a preproc step
-    sent1 = set([tok.text.lower() for tok in nlp(sent1)])
-    sent2 = set([tok.text.lower() for tok in nlp(sent2)])
+    sent1 = set([tok.text.lower() for tok in sent1])
+    sent2 = set([tok.text.lower() for tok in sent2])
     score = len(sent1 & sent2)
     return score
+
+
+
