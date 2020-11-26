0648d82f87e70ee0717b1105bf0926128aac3842
kasparvonbeelen <kbeelen@lwm-embeddings.kebr4boiy0luzjt0lpaozyeibd.ax.internal.cloudapp.net>
2020-11-26 17:04:31 +0000

    explain provenance of quotations data

--- bert_nn_demo.py
+++ bert_nn_demo.py
@@ -3,17 +3,20 @@ from pathlib import Path
 from flair.embeddings import TransformerWordEmbeddings
 
 def test_bert_001(model_name: str='bert-base-uncased',
-                  layers: str="-1",
-                  pooling_operation: str="mean",
-                  dataframe_file: str="./data/quotations_all_machine_nn01.pickle",
-                  word_id: str="machine_nn01"):
+                layers: str="-1",
+                pooling_operation: str="mean",
+                dataframe_file: str="./data/quotations_all_machine_nn01.pickle",
+                word_id: str="machine_nn01"):
 
     print(f"[INFO] Prepare data using {model_name}.")
     embedding_type = TransformerWordEmbeddings(model_name,
-                                               layers=layers,
-                                               pooling_operation=pooling_operation)
+                                            layers=layers,
+                                            pooling_operation=pooling_operation)
 
+    # path should point to a file with all quotations
+    # this file was created by the harvest_quotations function
     path = Path(dataframe_file)
+
     quotations = prepare_data(path, embedding_type)
 
     print(f"[INFO] Extract quotations for word_id: {word_id}")    
