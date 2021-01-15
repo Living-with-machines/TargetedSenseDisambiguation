542dfbf6a935a4c44d50e387114bac720edc6965
kasparvonbeelen <kbeelen@lwm-embeddings.kebr4boiy0luzjt0lpaozyeibd.ax.internal.cloudapp.net>
2021-01-15 10:55:03 +0000

    add simple command line glossbert

---
+++ run_glossbert.py
@@ -0,0 +1,10 @@
+from tasks.wsd_gloss import create_glossbert_data, train_glossbert
+import sys
+
+def run(lemma,pos):
+    data_path = create_glossbert_data(lemma,pos)
+    train_glossbert(data_path)
+
+if __name__=="__main__":
+    lemma,pos = sys.argv[1],sys.argv[2]
+    run(lemma,pos)
